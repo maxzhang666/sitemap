ce265bf2382ac906fc3ba462556d4d41f6040b78
MaxZhang <373884384@qq.com>
2022-09-13 11:45:39 +0800

    配置更新

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,6 +1,6 @@
 import app from 'flarum/admin/app';
 import SitemapSettingsPage from './components/SitemapSettingsPage';
 
-app.initializers.add('fof/sitemap', () => {
-  app.extensionData.for('fof-sitemap').registerPage(SitemapSettingsPage);
+app.initializers.add('maxzhang/sitemap', () => {
+  app.extensionData.for('maxzhang-sitemap').registerPage(SitemapSettingsPage);
 });
