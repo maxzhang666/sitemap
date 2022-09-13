4f5a0f52e4b2fdce667e76e6c7332594ca3d0c3d
MaxZhang <373884384@qq.com>
2022-09-13 12:05:06 +0800

    js调整

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,6 +1,6 @@
 import app from 'flarum/admin/app';
 import SitemapSettingsPage from './components/SitemapSettingsPage';
 
-app.initializers.add('maxzhang/sitemap', () => {
+app.initializers.add('maxzhang-sitemap', () => {
   app.extensionData.for('maxzhang-sitemap').registerPage(SitemapSettingsPage);
 });
